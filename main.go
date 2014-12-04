81dc6b3666d4db77e362b67f8e30f6dcf53d212b
Andraz Vrhovec <andraz@koofr.net>
2014-12-04 21:22:31 +0100

    Some GC opts

--- main.go
+++ main.go
@@ -84,7 +84,7 @@ func makeData(size int) (data []float32) {
 	data = make([]float32, size)
 
 	for i := 0; i < size; i++ {
-		data[i] = rand.Float32()
+		data[i] = rand.Float32() * 50
 	}
 	return
 }
@@ -94,35 +94,30 @@ var tpl = `
 <html>
 <body>
 
-<canvas id="myCanvas" width="1000" height="500"
-style="border:1px solid #d3d3d3;">
+<canvas id="myCanvas" width="1000" height="500" style="display: fixed; border:1px solid #d3d3d3;">
 Your browser does not support the HTML5 canvas tag.</canvas>
 <div id="fps"></div>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-
 <script>
     var c = document.getElementById("myCanvas");
+    var fpsDiv = document.getElementById("fps");
     var ctx = c.getContext("2d");
 
-    var data = null;
-
+    var data = new Float32Array(5000);
 
     conn = new WebSocket("ws://" + window.location.host + "/ws");
     conn.binaryType = "arraybuffer"
-
 
     last = new Date().getTime()
 
     function drawLine(data, y) {
         ctx.moveTo(0,y);
 
-        for (i = 0; i < data.length; i++) { 
-            var dva = y + (data[i] * 50)
+        for (i = 0; i < data.length; i=i+2) { 
+            var dva = Math.floor(y + data[i])
             var ena = i;
             ctx.lineTo(ena,dva);
             ctx.moveTo(ena,dva);
-            
         }
     }
 
@@ -130,15 +125,7 @@ Your browser does not support the HTML5 canvas tag.</canvas>
 
         ctx.beginPath();
         ctx.clearRect(0,0,1000,500);
-        // ctx.moveTo(0,0);
 
-        // for (i = 0; i < 1024; i++) { 
-        //     var dva = data[i] * 500
-        //     var ena = i;
-        //     ctx.lineTo(ena,dva);
-        //     ctx.moveTo(ena,dva);
-            
-        // }
         ctx.strokeStyle="#000000";
         drawLine(data.subarray(0,999), 0)
         ctx.stroke();
@@ -160,31 +147,29 @@ Your browser does not support the HTML5 canvas tag.</canvas>
         ctx.stroke();
 
         ctx.closePath();
-        
-
-        // window.requestAnimationFrame(function() {
-        //     window.requestAnimationFrame(draw);
-        // })
     }
-    // draw();
+
+    var cmd = "foo"
 
     conn.onopen = function() {
-        conn.send("foo")
+        conn.send(cmd)
     }
 
     conn.onmessage = function(e) {
-        data = new Float32Array(e.data)
-        // draw()
+        // data = new Float32Array(e.data)
+        data.set(new Float32Array(e.data))
+
         window.requestAnimationFrame(function() {
-            draw();
-            conn.send("foo")
+
+            draw()
+            conn.send(cmd)
 
             delta = (new Date().getTime() - last)/1000
             fps = 1/delta
             last = new Date().getTime()
-            $("#fps").text(fps)
+
+            fpsDiv.innerText = fps
         })
-        
 
 
     }
